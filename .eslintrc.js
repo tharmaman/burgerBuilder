f5aa396320fa94a450674e54a581ceb509b9fa22
tharmaman <daniel.tharmathurai@mail.mcgill.ca>
2018-09-24 19:16:08 -0400

    fixed up eslint

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    "extends": "airbnb",
+    "rules": {
+        // remove random jsx error
+        "react/jsx-filename-extension": 0,
+
+        // Indent with 4 spaces
+        "indent": ["error", 4],
+
+        // Indent JSX with 4 spaces
+        "react/jsx-indent": ["error", 4],
+
+        // Indent props with 4 spaces
+        "react/jsx-indent-props": ["error", 4],
+
+        // allow console.log for debugger
+        "no-console": 0,
+
+        // disable prop types
+        "react/prop-types": 0,
+
+        // disable camel case
+        "camelcase": 0,
+
+        // disable prefer export default
+        "import/prefer-default-export": 0,
+
+        // remove no-shadow
+        "no-shadow": 0,
+    },
+};
