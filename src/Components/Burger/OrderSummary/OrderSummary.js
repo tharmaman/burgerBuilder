7a73388eb1150846d66e317b9daa739d93f2b9ed
tharmaman <daniel.tharmathurai@mail.mcgill.ca>
2018-09-25 11:44:24 -0400

    added shouldComponentUpdate in Modal

--- src/Components/Burger/OrderSummary/OrderSummary.js
+++ src/Components/Burger/OrderSummary/OrderSummary.js
@@ -1,51 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../../hocs/Aux';
 import Button from '../../UI/Button/Button';
 
 // eslint-disable-next-line
-const orderSummary = props => {
-    const ingredientSummary = Object.keys(props.ingredients)
-        .map(ingKey => (
-            <li key={ingKey}>
-                <span
-                    style={{ textTransform: 'capitalize' }}
+class OrderSummary extends Component {
+
+    componentWillUpdate() {
+        console.log('[OrderSummary] WillUpdate');
+    }
+
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients)
+            .map(ingKey => (
+                <li key={ingKey}>
+                    <span
+                        style={{ textTransform: 'capitalize' }}
+                    >
+                        {/* // eslint-disable-next-line */}
+                        {ingKey}
+                        :
+                        {this.props.ingredients[ingKey]}
+                    </span>
+                </li>
+            ));
+        return (
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p>
+                    <strong>
+                        {/* eslint-disable-next-line */}
+                        Total Price: {this.props.price.toFixed(2)}
+                    </strong>
+                </p>
+                <p>Continue to Checkout?</p>
+                <Button
+                    btnType="Danger"
+                    clicked={this.props.purchaseCanceled}
                 >
-                    {/* // eslint-disable-next-line */}
-                    {ingKey}
-                    :
-                    {props.ingredients[ingKey]}
-                </span>
-            </li>
-        ));
+                    CANCEL
+                </Button>
+                <Button
+                    btnType="Success"
+                    clicked={this.props.purchaseContinued}
+                >
+                    CONTINUE
+                </Button>
+            </Aux>
+        );
+    }
+}
 
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p>
-                <strong>
-                    {/* eslint-disable-next-line */}
-                    Total Price: {props.price.toFixed(2)}
-                </strong>
-            </p>
-            <p>Continue to Checkout?</p>
-            <Button
-                btnType="Danger"
-                clicked={props.purchaseCanceled}
-            >
-                CANCEL
-            </Button>
-            <Button
-                btnType="Success"
-                clicked={props.purchaseContinued}
-            >
-                CONTINUE
-            </Button>
-        </Aux>
-    );
-};
-
-export default orderSummary;
+export default OrderSummary;
