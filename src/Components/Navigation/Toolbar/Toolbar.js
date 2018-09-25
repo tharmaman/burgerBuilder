1810137f9a662a14d7b1e0919ce3c88df111893a
tharmaman <daniel.tharmathurai@mail.mcgill.ca>
2018-09-25 01:30:38 -0400

    fucking nav button

--- src/Components/Navigation/Toolbar/Toolbar.js
+++ src/Components/Navigation/Toolbar/Toolbar.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Logo from '../../Logo/Logo';
 import classes from './Toolbar.css';
+import NavigationItems from '../NavigationItems/NavigationItems';
 
 const toolbar = () => (
     <header className={classes.Toolbar}>
         <div>MENU</div>
         <Logo />
         <nav>
-            ...
+            <NavigationItems />
         </nav>
     </header>
 );
