075dd7fec1147d0e7810208c7cf685afd8a70963
tharmaman <daniel.tharmathurai@mail.mcgill.ca>
2018-09-25 01:38:26 -0400

    created responsive sidedrawer

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Aux from '../../hocs/Aux';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = ({ children }) => (
     <Aux>
         <Toolbar />
+        <SideDrawer />
         <div>Toolbar, SideDrawer, Backdrop</div>
         <main className={classes.Content}>
             {children}
