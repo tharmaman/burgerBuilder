bd85775d33504acac29305a87eb9c32cf969939a
tharmaman <daniel.tharmathurai@mail.mcgill.ca>
2018-09-26 01:39:11 -0400

    connected to firebase

--- src/Containers/BurgerBuilder/BurgerBuilder.js
+++ src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../Components/Burger/Burger';
 import BuildControls from '../../Components/Burger/BuildControls/BuildControls';
 import Modal from '../../Components/UI/Modal/Modal';
 import OrderSummary from '../../Components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders'; // our axios instance to firebase
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -89,8 +90,24 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        // eslint-disable-next-line
-        alert('You continue!');
+        // alert('You continue!');
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Danny Tharmz',
+                address: {
+                    street: 'Teststreet 1',
+                    zipCode: '41231',
+                    country: 'Germany',
+                },
+                email: 'test@test.com',
+            },
+            deliveryMethod: 'fastest',
+        };
+        axios.post('./orders.json', order) // for firebase always remember .json
+            .then(response => console.log(response))
+            .catch(error => console.log(error));
     }
 
     render() {
