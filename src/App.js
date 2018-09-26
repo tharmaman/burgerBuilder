d63ca9fb79f0519bc18ee60778e71acb124507bd
tharmaman <daniel.tharmathurai@mail.mcgill.ca>
2018-09-26 03:23:24 -0400

    finished up error handling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Layout from './hocs/Layout/Layout';
 import BurgerBuilder from './Containers/BurgerBuilder/BurgerBuilder';
 
-// class App extends Component {
-//     render() {
-//         return (
-//             <div>
-//                 <Layout>
-//                     Test
-//                 </Layout>
-//             </div>
-//         );
-//     }
-// }
+// eslint-disable-next-line
+class App extends Component {
+    // state = {
+    //     show: true,
+    // }
 
-const App = () => (
-    <div>
-        <Layout>
-            <BurgerBuilder />
-        </Layout>
-    </div>
-);
+    // componentDidMount() {
+    //     setTimeout(() => {
+    //         this.setState({
+    //             show: false,
+    //         });
+    //     }, 5000);
+    // }
 
+    render() {
+        return (
+            <div>
+                <Layout>
+                    <BurgerBuilder />
+                </Layout>
+            </div>
+        );
+    }
+}
 
 export default App;
--- src/hocs/withErrorHandler/withErrorHandler.js
+++ src/hocs/withErrorHandler/withErrorHandler.js
@@ -11,17 +11,27 @@ const withErrorHandler = (WrappedComponent, axios) => {
         }
 
         componentWillMount() {
-            axios.interceptors.request.use((req) => {
+            this.reqInterceptor = axios.interceptors.request.use((req) => {
                 this.setState({
                     error: null,
                 });
                 return req;
             });
-            axios.interceptors.response.use(res => res, (error) => {
+            this.resInterceptor = axios.interceptors.response.use(res => res, (error) => {
                 this.setState({
                     error,
                 });
             });
+        }
+
+        // ! for when we don't need the burger builder component anymore => important later on
+        // ejecting the interceptors once component is monuted
+        // disappears after a while
+        // removing intereceptors which prevent memory links
+        componentWillUnmount() {
+            console.log('Will Unmount', this.reqInterceptor, this.resInterceptor);
+            axios.interceptors.request.eject(this.reqInterceptor);
+            axios.interceptors.response.eject(this.resInterceptor);
         }
 
         errorConfirmedHandler = () => {
