bababa61cb091eaf13171fabd133da888b891723
tharmaman <daniel.tharmathurai@mail.mcgill.ca>
2018-09-27 02:52:13 -0400

    fixed eslint shit

--- src/Containers/Orders/Orders.js
+++ src/Containers/Orders/Orders.js
@@ -1,16 +1,52 @@
 import React, { Component } from 'react';
 
+import axios from '../../axios-orders';
 import Order from '../../Components/Order/Order';
+import withErrorHandler from '../../hocs/withErrorHandler/withErrorHandler';
 
 class Orders extends Component {
+    state = {
+        orders: [],
+        loading: true,
+    }
+
+    componentDidMount() {
+        axios.get('/orders.json')
+            .then((res) => {
+                const fetchedOrders = [];
+
+                // eslint-disable-next-line
+                for (let key in res.data) {
+                    fetchedOrders.push({
+                        ...res.data[key],
+                        id: key,
+                    });
+                }
+                this.setState({
+                    loading: false,
+                    orders: fetchedOrders,
+                });
+            })
+            .catch(() => {
+                this.setState({
+                    loading: false,
+                });
+            });
+    }
+
     render() {
         return (
             <div>
-                <Order />
-                <Order />
+                {this.state.orders.map(order => (
+                    <Order
+                        key={order.id}
+                        ingredients={order.ingredients}
+                        price={+order.price} // adding plus converts it to number
+                    />
+                ))}
             </div>
         );
     }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
