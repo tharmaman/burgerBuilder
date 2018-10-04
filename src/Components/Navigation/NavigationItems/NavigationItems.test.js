620ce6b5e4c3fb97436731e48a80472b45d1bbef
tharmaman <daniel.tharmathurai@mail.mcgill.ca>
2018-10-04 14:13:51 -0400

    wrote basic tests using setProps

--- src/Components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/Components/Navigation/NavigationItems/NavigationItems.test.js
@@ -9,8 +9,21 @@ import NavigationItem from './NavigationItem/NavigationItem';
 configure({ adapter: new Adapter() });
 
 describe('<NavigationItems />', () => {
-    it('should render two <NaviigationItem /> elements if not authenticated', () => {
-        const wrapper = shallow(<NavigationItems />);
+    let wrapper;
+
+    beforeEach(() => {
+        wrapper = shallow(<NavigationItems />);
+    });
+
+    it('should render two <NavigationItem /> elements if not authenticated', () => {
         expect(wrapper.find(NavigationItem)).toHaveLength(2);
+    });
+
+    it('should render three <NavigationItem /> elements if authenticated', () => {
+        // wrapper = shallow(<NavigationItems isAuthenticated />);
+        wrapper.setProps({
+            isAuthenticated: true,
+        });
+        expect(wrapper.find(NavigationItem)).toHaveLength(3);
     });
 });
